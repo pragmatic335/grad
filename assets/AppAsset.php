3dfec566e0d60de0960434be4ed948e2cac6a355
pragmatic335 <pragmatik335gmail.com>
2020-07-21 16:52:34 +0700

    qwerty

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -26,6 +26,8 @@ class AppAsset extends AssetBundle
     ];
     public $depends = [
         'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
 
     ];
 }
